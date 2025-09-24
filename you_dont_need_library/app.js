e348d1e2bd2d3ed6956e14c39d68d18b9c519be7
jpoliveira08 <joaopedro.oliveirajava@gmail.com>
2025-09-23 23:21:11 -0300

    no-framework: Browser Routing & History API

---
+++ you_dont_need_library/app.js
@@ -0,0 +1,9 @@
+import Store from './services/Store.js';
+import Router from './services/Router.js';
+
+window.app = {}
+app.store = Store;
+app.router = Router;
+
+window.addEventListener("DOMContentLoaded", async () => {
+});
---
+++ you_dont_need_library/services/Router.js
@@ -0,0 +1,10 @@
+const Router = {
+    init: () => {
+
+    },
+    go: (route, addToHistory = true) => {
+        console.log(`Going to ${route}`);
+    }
+}
+
+export default Router;
