60a4877cffff6e406bd69bb7af185f1655479236
jpoliveira08 <joaopedro.oliveirajava@gmail.com>
2025-10-17 19:45:53 -0300

    you_dont_need_library-changing-dom-element-content

--- you_dont_need_library/services/Router.js
+++ you_dont_need_library/services/Router.js
@@ -18,8 +18,37 @@ const Router = {
         console.log(`Going to ${route}`);
 
         if (addToHistory) {
-            history,pushState({ route }, null, route);
+            // You can fake url
+            // history.pushState({ route }, null,'/something/something/' + route);
+            history.pushState({ route }, null, route);
         }
+
+        let pageElement = null;
+        switch (route) {
+            // Remove from the DOM and inset other element on it
+            // Based on the <main> element inject/remove dom elements
+            case "/":
+                pageElement = document.createElement("h1");
+                pageElement.textContent = "Menu"
+                break
+            case "/order":
+                pageElement = document.createElement("h1");
+                pageElement.textContent = "Your Order"
+                break;
+        }
+        if (pageElement) {
+            document.querySelector("main").children[0]?.remove();
+
+            // Quick dirty way to clean the HTML element - innerHTML = ""
+            document.querySelector("main").appendChild(pageElement);
+        }
+
+        // Good idea when setting a router to set scrollX and scrollY to 0
+        // Because for large page with long content, the user might be at the 
+        // bottom of the page and when the router is called, the user will be 
+        // at the bottom of the page
+        window.scrollX = 0;
+        window.scrollY = 0;
     }
 }
 
